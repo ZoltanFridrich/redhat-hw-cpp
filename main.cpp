6e0ba651d23f3d912aa6c8c54ae5772eb313f95d
Zoltan Fridrich <445620@mail.muni.cz>
2019-05-18 16:14:11 +0200

    polish

--- main.cpp
+++ main.cpp
@@ -10,15 +10,15 @@ struct Item {
     size_t counter;
 };
 
-auto cmp = [](const Item &lhs, const Item &rhs) {
+auto itemCmp = [](const Item &lhs, const Item &rhs) {
     return lhs.priority == rhs.priority ? lhs.counter > rhs.counter : lhs.priority < rhs.priority;
 };
 
-using buffer = typename std::priority_queue<Item, std::vector<Item>, decltype(cmp)>; 
+using Buffer = typename std::priority_queue<Item, std::vector<Item>, decltype(itemCmp)>; 
 
 enum class InputType { unknown, push, print, get, exit };
 
-InputType parseInput(std::string &&str, Item &item) {
+InputType parseInput(const std::string &str, Item &item) {
     if (str == "exit") return InputType::exit;
     if (str == "print") return InputType::print;
     if (str == "get") return InputType::get;
@@ -32,15 +32,15 @@ InputType parseInput(std::string &&str, Item &item) {
     return InputType::unknown;
 }
 
-inline void printTop(const buffer &buf) {
+inline void printTop(const Buffer &buf) {
     std::cout << buf.top().value << ':' << buf.top().priority << std::endl;
 }
 
 int main() {
-    buffer buf(cmp);
+    Buffer buf(itemCmp);
     for (std::string input; std::getline(std::cin, input);) {
 	Item item;
-	switch (parseInput(std::move(input), item)) {
+	switch (parseInput(input, item)) {
 	case InputType::push:
 	    buf.push(item);
 	    break;
